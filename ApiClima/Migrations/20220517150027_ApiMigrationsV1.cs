c9fdff4103e9d7396f187211c35456afdc2553d8
AyrtonFidel9 <ayrtonfidel9@gmail.com>
2022-05-17 10:07:03 -0500

    Migrations added

---
+++ ApiClima/Migrations/20220517150027_ApiMigrationsV1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiClima.Migrations
+{
+    public partial class ApiMigrationsV1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Climas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    pais = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    ciudad = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    probPrecipitaciones = table.Column<double>(type: "float", nullable: true),
+                    humedad = table.Column<double>(type: "float", nullable: true),
+                    viento = table.Column<double>(type: "float", nullable: true),
+                    sensacion = table.Column<int>(type: "int", nullable: true),
+                    descripcion = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Climas", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Climas");
+        }
+    }
+}
